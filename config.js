76bce3a53e41f5f7bd9b1539e924ec9466b4ffb4
Anis Assaidi <anis.assaidi@student.vinci.be>
2022-11-28 11:56:36 +0100

    change db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const PgUser = 'postgres';
 const PgHost = 'localhost';
-const PgDb = 'exoplanets';
+const PgDb = 'postgres://raytylyn:urz8BXUL064NPASQrqW77ErU62ebN7Fp@ella.db.elephantsql.com/raytylyn';
 const PgPassword = 'ipl';
 const PgPort = 5432;
 
