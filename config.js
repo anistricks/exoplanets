5b8ff25c17f29ef3a4cc3898232f57da11edb9c4
Anis Assaidi <anis.assaidi@student.vinci.be>
2022-11-28 12:19:41 +0100

    changement package.json

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-const PgUser = 'postgres';
-const PgHost = 'localhost';
-const PgDb = 'exoplanets';
-const PgPassword = 'ipl';
-const PgPort = 5432;
+//const PgUser = 'postgres';
+//const PgHost = 'localhost';
+//const PgDb = 'exoplanets';
+//const PgPassword = 'ipl';
+//const PgPort = 5432;
 
 module.exports = {
-    ConnectionString: 'postgres://' + PgUser + ':' + PgPassword + '@' + PgHost + ':' + PgPort + '/' + PgDb
+    //ConnectionString: 'postgres://' + PgUser + ':' + PgPassword + '@' + PgHost + ':' + PgPort + '/' + PgDb
+    ConnectionString: process.env.DATABASE_URL
 };
